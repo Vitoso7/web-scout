6ec97e8ea80b28c5f69844d9fc6aed57092b6fba
Vitoso7 <victorhugodelisboa@gmail.com>
2024-02-09 17:04:53 -0300

    feat: docker compose and eslint generic array rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     extends: [
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/stylistic',
     ],
     root: true,
     env: {
@@ -21,5 +22,7 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     },
 };
