79642fee94a63cd8e5050cd28839473dcfe12653
Leonardo Freitas <leonardo.paulino.ps@ojc.com.br>
2024-02-11 17:36:12 -0300

    feat: rabbit mq setup

--- src/headless-browser/headless-browser.service.spec.ts
+++ src/headless-browser/headless-browser.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HeadlessBrowserService } from './headless-browser.service';
 
 describe('HeadlessBrowserService', () => {
-  let service: HeadlessBrowserService;
+    let service: HeadlessBrowserService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [HeadlessBrowserService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [HeadlessBrowserService],
+        }).compile();
 
-    service = module.get<HeadlessBrowserService>(HeadlessBrowserService);
-  });
+        service = module.get<HeadlessBrowserService>(HeadlessBrowserService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
